367991df322325a58ec4bb7f364a214eafd13175
aqua73438274 <155337168+aqua73438274@users.noreply.github.com>
2024-10-19 14:23:16 +0800

    for  not enough values to unpack (expected 2, got 1)

--- qmk_firmware/keyboards/keyball/keyball44/keyball44.h
+++ qmk_firmware/keyboards/keyball/keyball44/keyball44.h
@@ -96,11 +96,10 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
     L10, L11, L12, L13, L14, L15,\
     L20, L21, L22, L23, L24, L25,\
          L31, L32, L33, L34, L35,\
-    \
-         R05, R04, R03, R02, R01, R00,\ 
-         R15, R14, R13, R12, R11, R10,\
-         R25, R24, R23, R22, R21, R20,\
-    R35, R34, R33, R32, R31       \
+    R05, R04, R03, R02, R01, R00,\ 
+    R15, R14, R13, R12, R11, R10,\
+    R25, R24, R23, R22, R21, R20,\
+    R35, R34, R33, R32, R31      \
     ) \
     { \
         {   L00,   L01,   L02,   L03,   L04,   L05 }, \
