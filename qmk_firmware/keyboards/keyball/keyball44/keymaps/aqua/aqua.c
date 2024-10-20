f3074348d428c4dd0e3b4bf8ef8d41e57f9e0d26
aqua73438274 <155337168+aqua73438274@users.noreply.github.com>
2024-10-20 21:18:47 +0800

    add layer color

--- qmk_firmware/keyboards/keyball/keyball44/keymaps/aqua/aqua.c
+++ qmk_firmware/keyboards/keyball/keyball44/keymaps/aqua/aqua.c
@@ -30,16 +30,18 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
   return process_record_user_a2j(keycode, record);
 }
 
+#ifdef POINTING_DEVICE_AUTO_MOUSE_ENABLE
 void pointing_device_init_user(void) {
     set_auto_mouse_layer(MOS);
     set_auto_mouse_enable(true);
 }
+#endif
 
 layer_state_t layer_state_set_user(layer_state_t state) {
   // レイヤーが1または3の場合、スクロールモードが有効になる
   keyball_set_scroll_mode(get_highest_layer(state) == 1 || get_highest_layer(state) == WIN);
   // keyball_set_scroll_mode(get_highest_layer(state) == 1);
-  
+
 #ifdef POINTING_DEVICE_AUTO_MOUSE_ENABLE
     keyball_handle_auto_mouse_layer_change(state);
 #endif
@@ -51,6 +53,12 @@ layer_state_t layer_state_set_user(layer_state_t state) {
       break;
     case NUM:
       rgblight_sethsv(HSV_RED);
+      break;
+    case FUN:
+      rgblight_sethsv(HSV_ORANGE);
+      break;
+    case WIN:
+      rgblight_sethsv(HSV_CHARTREUSE);
       break;
     case SYM:
       rgblight_sethsv(HSV_YELLOW);
