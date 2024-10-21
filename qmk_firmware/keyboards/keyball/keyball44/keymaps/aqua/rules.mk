70d1ade4153ba2b394042fb44ef6438935ca833e
aqua73438274 <155337168+aqua73438274@users.noreply.github.com>
2024-10-21 20:48:28 +0800

    make qwerty

--- qmk_firmware/keyboards/keyball/keyball44/keymaps/aqua/rules.mk
+++ qmk_firmware/keyboards/keyball/keyball44/keymaps/aqua/rules.mk
@@ -3,12 +3,12 @@ RGBLIGHT_ENABLE = yes
 OLED_ENABLE = no
 
 # for debug
-#CONSOLE_ENABLE = yes
+# CONSOLE_ENABLE = yes
 
 
 
 # aqua settings
-ALT_KEYMAP_DISEBLE	# for use qwerty layout
+ALT_KEYMAP_DISEBLE = yes	# for use qwerty layout
 COMBO_ENABLE = yes
 SRC += features/translate_ansi_to_jis.c
 
