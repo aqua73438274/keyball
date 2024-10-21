1c4de900fca4edf1dc4e5a22739b53619ae79b7a
aqua73438274 <155337168+aqua73438274@users.noreply.github.com>
2024-10-21 22:25:20 +0800

    add ACHORDION_ENABLE

--- qmk_firmware/keyboards/keyball/keyball44/keymaps/aqua/rules.mk
+++ qmk_firmware/keyboards/keyball/keyball44/keymaps/aqua/rules.mk
@@ -8,8 +8,15 @@ OLED_ENABLE = no
 
 
 # aqua settings
+AUTOCORRECT_ENABLE ?= yes
 ALT_KEYMAP_DISEBLE = yes	# for use qwerty layout
 COMBO_ENABLE = yes
 SRC += features/translate_ansi_to_jis.c
 
 # CAPS_WORD_ENABLE ?= yes
+
+ACHORDION_ENABLE ?= yes
+ifeq ($(strip $(ACHORDION_ENABLE)), yes)
+	OPT_DEFS += -DACHORDION_ENABLE
+	SRC += features/achordion.c
+endif
--- qmk_firmware/keyboards/keyball/keyball61/keymaps/aqua/rules.mk
+++ qmk_firmware/keyboards/keyball/keyball61/keymaps/aqua/rules.mk
@@ -9,7 +9,15 @@ OLED_ENABLE = no
 
 # aqua settings
 #ALT_KEYMAP_DISEBLE #for use qwerty layout
+ACHORDION_ENABLE ?= yes
 COMBO_ENABLE = yes
 SRC += features/translate_ansi_to_jis.c
 
 # CAPS_WORD_ENABLE ?= yes
+
+ACHORDION_ENABLE ?= yes
+ifeq ($(strip $(ACHORDION_ENABLE)), yes)
+	OPT_DEFS += -DACHORDION_ENABLE
+	SRC += features/achordion.c
+endif
+
