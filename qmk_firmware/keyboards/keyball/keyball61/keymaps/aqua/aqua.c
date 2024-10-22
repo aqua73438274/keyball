bf8a4e6b5c5b645722c6a4f35b8d10455484d369
aqua73438274 <155337168+aqua73438274@users.noreply.github.com>
2024-10-22 21:45:25 +0800

    add caps word

--- qmk_firmware/keyboards/keyball/keyball61/keymaps/aqua/aqua.c
+++ qmk_firmware/keyboards/keyball/keyball61/keymaps/aqua/aqua.c
@@ -163,4 +163,25 @@ layer_state_t layer_state_set_user(layer_state_t state) {
   return state;
 }
 
+#ifdef CAPS_WORD_ENABLE
+bool caps_word_press_user(uint16_t keycode) {
+  switch (keycode) {
+    // Keycodes that continue Caps Word, with shift applied.
+    case KC_A ... KC_Z:
+      add_weak_mods(MOD_BIT(KC_LSFT));  // Apply shift to the next key.
+      return true;
+
+    // Keycodes that continue Caps Word, without shifting.
+    case KC_1 ... KC_0:
+    case KC_BSPC:
+    case KC_DEL:
+    case KC_UNDS:
+      return true;
+
+    default:
+      return false;  // Deactivate Caps Word.
+  }
+}
+#endif  // CAPS_WORD_ENABLE
+
 #endif // _AQUA_C_
