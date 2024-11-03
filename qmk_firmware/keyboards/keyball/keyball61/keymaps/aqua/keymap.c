c488bd43b7da762dc939d9cc91bc94775d2689b8
aqua73438274 <155337168+aqua73438274@users.noreply.github.com>
2024-11-03 17:35:44 +0800

    fix keymap

--- qmk_firmware/keyboards/keyball/keyball61/keymaps/aqua/keymap.c
+++ qmk_firmware/keyboards/keyball/keyball61/keymaps/aqua/keymap.c
@@ -29,7 +29,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     , B_1_L1 , B_1_L2 , B_1_L3 , B_1_L4 , B_1_L5 , B_1_L6
     , B_2_L1 , B_2_L2 , B_2_L3 , B_2_L4 , B_2_L5 , B_2_L6
     , B_3_L1 , B_3_L2 , B_3_L3 , B_3_L4 , B_3_L5 , B_3_L6 , B_3_L7
-    , B_4_L1 , B_4_L2 , B_4_L2 , B_4_L3 , B_4_L4 , B_4_L5 , B_4_L7
+    , B_4_L1 , B_4_L2 , B_4_L3 , B_4_L4 , B_4_L5 , B_4_L6 , B_4_L7
    
     ,                   KC_F6  , KC_F7  , KC_F8  , KC_F9  , KC_F10 , KC_F11
     ,                   B_1_R1 , B_1_R2 , B_1_R3 , B_1_R4 , B_1_R5 , B_1_R6
