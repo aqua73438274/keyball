00c5c7fe5ee22511b7c493346ac419e9f218fc97
aqua73438274 <155337168+aqua73438274@users.noreply.github.com>
2024-11-05 22:27:59 +0800

    For error: right-hand operand

--- qmk_firmware/keyboards/keyball/keyball61/keymaps/aqua/aqua.c
+++ qmk_firmware/keyboards/keyball/keyball61/keymaps/aqua/aqua.c
@@ -97,7 +97,7 @@ uint16_t achordion_streak_chord_timeout(
 // held while sending the string. Additionally, the last key is set such that if
 // the Repeat Key is pressed next, it produces `repeat_keycode`.
 #define MAGIC_STRING(str, repeat_keycode) \
-        (PSTR(str), (repeat_keycode))
+        magic_send_string_P(PSTR(str), (repeat_keycode))
  
 static void magic_send_string_P(const char* str, uint16_t repeat_keycode) {
     uint8_t saved_mods = 0;
