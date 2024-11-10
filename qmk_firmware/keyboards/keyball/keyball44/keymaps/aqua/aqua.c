b186aceb53ea95b241f94dc26cb0749927a2f1ac
aqua73438274 <155337168+aqua73438274@users.noreply.github.com>
2024-11-10 21:38:59 +0800

    fix achodhion

--- qmk_firmware/keyboards/keyball/keyball44/keymaps/aqua/aqua.c
+++ qmk_firmware/keyboards/keyball/keyball44/keymaps/aqua/aqua.c
@@ -153,7 +153,7 @@ static void process_right_magic(uint16_t keycode, uint8_t mods) { // LMAGIC defi
         case HOM_T: { MAGIC_STRING("k",         KC_NO); } break;
         case  KC_U: { MAGIC_STRING("u",         KC_NO); } break;
         case  KC_V: { MAGIC_STRING("x",         KC_NO); } break;
-        case  KC_W: { MAGIC_STRING("c",         KC_NO); } break;
+        case NUM_W: { MAGIC_STRING("c",         KC_NO); } break;
         case HOM_X: { MAGIC_STRING("l",         KC_NO); } break;
         case HOM_Y: { MAGIC_STRING("d",         KC_NO); } break;
         case  KC_Z: { MAGIC_STRING("y",         KC_NO); } break;
@@ -186,7 +186,7 @@ static void process_left_magic(uint16_t keycode, uint8_t mods) { // RMAGIC defin
         case HOM_T: { MAGIC_STRING("t",         KC_NO); } break;
         case  KC_U: { MAGIC_STRING("i",         KC_NO); } break;
         case  KC_V: { MAGIC_STRING("v",         KC_NO); } break;
-        case  KC_W: { MAGIC_STRING("w",         KC_NO); } break;
+        case NUM_W: { MAGIC_STRING("w",         KC_NO); } break;
         case HOM_X: { MAGIC_STRING("x",         KC_NO); } break;
         case HOM_Y: { MAGIC_STRING("y",         KC_NO); } break;
         case  KC_Z: { MAGIC_STRING("z",         KC_NO); } break;
