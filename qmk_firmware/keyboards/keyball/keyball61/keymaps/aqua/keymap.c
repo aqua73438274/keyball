397fb796c054ed8ae984b5c744ed080745599e7a
aqua73438274 <155337168+aqua73438274@users.noreply.github.com>
2024-11-10 18:22:50 +0800

    add shortcut

--- qmk_firmware/keyboards/keyball/keyball61/keymaps/aqua/keymap.c
+++ qmk_firmware/keyboards/keyball/keyball61/keymaps/aqua/keymap.c
@@ -98,7 +98,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
       _______   , _______, _______  , _______ , _______  , _______
     , _______   , XXXXXXX, XXXXXXX  , XXXXXXX , XXXXXXX  , XXXXXXX
     , _______   , KC_LGUI, KC_LALT  , KC_LSFT , KC_LCTL  , KC_TAB
-    , _______   , XXXXXXX, XXXXXXX  , KC_LCTL , XXXXXXX  , XXXXXXX , XXXXXXX
+    , _______   , C(KC_Z), C(KC_X)  , C(KC_C) , C(KC_V)  , XXXXXXX , XXXXXXX
     , _______   , _______,  KC_LEFT , KC_RGHT ,C(KC_PGUP), _______ ,C(KC_PGDN)
 
     ,                   _______, _______, _______, _______, _______ , _______
