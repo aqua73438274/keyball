21b15a7763551c555f162b1e0c264f9d19c1cb75
aqua73438274 <155337168+aqua73438274@users.noreply.github.com>
2024-11-10 18:16:17 +0800

    For Error

--- qmk_firmware/keyboards/keyball/keyball61/keymaps/aqua/config_aqua.h
+++ qmk_firmware/keyboards/keyball/keyball61/keymaps/aqua/config_aqua.h
@@ -122,20 +122,6 @@ enum {
 //    ,B_3_R0=KC_LNG1, B_3_R1= KC_G  , B_3_R2= KC_D  , B_3_R3= KC_MS_BTN1, B_3_R4= KC_J  , B_3_R5= HOM_MS   , B_3_R6= KC_QUOT
 //    ,B_4_R0= NUM_BS, B_4_R1= NAV_ET, B_4_R2= KC_NO , B_4_R3= KC_NO     , B_4_R4= KC_NO , B_4_R5=KC_MS_BTN2, B_4_R6= KC_NO
 // };
-
-    //Left
-    B_1_L1= KC_DLR , B_1_L2= KC_Q  , B_1_L3= KC_U  , B_1_L4= KC_O    , B_1_L5= KC_F  , B_1_L6= KC_P
-   ,B_2_L1= KC_BSPC, B_2_L2= HOM_E , B_2_L3= HOM_I , B_2_L4= HOM_A   , B_2_L5= HOM_N , B_2_L6= KC_H
-   ,B_3_L1= KC_AT  , B_3_L2= HOM_SC, B_3_L3=KC_COMM, B_3_L4= KC_DOT  , B_3_L5= NUM_MN, B_3_L6= KC_B, B_3_L7= ALT_T(KC_LNG2)
-   ,B_4_L1=KC_LEFT , B_4_L2= WIN_DW, B_4_L3= WIN_UP, B_4_L4=KC_RGHT  , B_4_L5= WIN_MN, B_4_L6= SFT_SP, B_4_L7= LMAGIC
-
-    //Right
-   ,                B_1_R1= KC_Z  , B_1_R2= KC_C  , B_1_R3= KC_L      , B_1_R4= KC_K  , B_1_R5= KC_V     , B_1_R6= KC_QUOT
-   ,                B_2_R1= KC_D  , B_2_R2= HOM_Y , B_2_R3= HOM_R     , B_2_R4= HOM_T , B_2_R5= HOM_S    , B_2_R6= KC_SLSH
-   ,B_3_R0=KC_LNG1, B_3_R1= KC_G  , B_3_R2= KC_W  , B_3_R3= KC_M      , B_3_R4= KC_J  , B_3_R5= HOM_X    , B_3_R6= KC_ENT
-   ,B_4_R0=RMAGIC, B_4_R1= NAV_ET, B_4_R2= KC_NO , B_4_R3= KC_NO      , B_4_R4= KC_NO , B_4_R5=GC(KC_LEFT),B_4_R6=GC(KC_RGHT)
-
-   
     //Left
     B_1_L1= KC_DLR , B_1_L2= KC_V  , B_1_L3= KC_K  , B_1_L4= KC_L    , B_1_L5= KC_C  , B_1_L6= KC_Z
    ,B_2_L1= KC_BSPC, B_2_L2= HOM_S , B_2_L3= HOM_T , B_2_L4= HOM_R   , B_2_L5= HOM_Y , B_2_L6= KC_D
