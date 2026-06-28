b7593d118a1e1cb3491dd49b18d4341969faf0c6
aqua73438274 <155337168+aqua73438274@users.noreply.github.com>
2026-06-28 13:59:32 +0800

    Update config.h

--- qmk_firmware/keyboards/keyball/keyball61/keymaps/aqua/config.h
+++ qmk_firmware/keyboards/keyball/keyball61/keymaps/aqua/config.h
@@ -36,5 +36,3 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #define POINTING_DEVICE_AUTO_MOUSE_ENABLE
 // only required if not setting mouse layer elsewhere
 #define AUTO_MOUSE_DEFAULT_LAYER 1
-
-//#define COMBO_COUNT 79
