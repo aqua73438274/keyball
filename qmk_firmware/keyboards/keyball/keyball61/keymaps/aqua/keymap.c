30c3a175e60df03f0f59857cb4057881b6a823ee
aqua73438274 <155337168+aqua73438274@users.noreply.github.com>
2026-07-11 22:31:48 +0800

    Update keymap.c

--- qmk_firmware/keyboards/keyball/keyball61/keymaps/aqua/keymap.c
+++ qmk_firmware/keyboards/keyball/keyball61/keymaps/aqua/keymap.c
@@ -176,10 +176,10 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     , _______, _______, _______, _______, KC_LCTL, _______ , TO(BASE)
    
     ,                   _______, _______, _______, _______, _______, _______
-    ,                   KC_TAB , KC_N7   , KC_N8   , KC_N9   , KC_PLUS, KC_SLSH
-    ,                   KC_COLN, KC_N1   , KC_N2   , KC_N3   , KC_ENT , KC_ASTR
-    ,         _______ , KC_COMM, KC_N4   , KC_N5   , KC_N6   , KC_DOT , _______
-    ,         LLOCK   , KC_0   , XXXXXXX, XXXXXXX, _______, _______, _______
+    ,                   KC_TAB , KC_P7   , KC_P8   , KC_P9   , KC_PLUS, KC_SLSH
+    ,                   KC_COLN, KC_P1   , KC_P2   , KC_P3   , KC_ENT , KC_ASTR
+    ,         _______ , KC_COMM, KC_P4   , KC_P5   , KC_P6   , KC_DOT , _______
+    ,         LLOCK   , KC_P0  , XXXXXXX, XXXXXXX, _______, _______, _______
   )
 
   ,[WIN] = LAYOUT_universal(
